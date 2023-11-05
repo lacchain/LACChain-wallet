c4bb3bfc7f131a2e9ad0071e56e7c22978c05bec
eum602 <eum602@gmail.com>
2023-11-05 10:31:07 -0500

    customize footer
    Signed-off-by: eum602 <eum602@gmail.com>

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import cn from 'classnames';
-import styles from './Footer.module.sass';
-import Theme from '../Theme';
+import React from "react";
+import cn from "classnames";
+import styles from "./Footer.module.sass";
 
 function Footers() {
   return (
     <footer className={styles.footer}>
-      <div className={cn('container', styles.container)}>
+      <div className={styles.footerimage}>
+        <img src="/images/content/footer-PAHO-IDB.png" alt="IDB-PAHO" />
+      </div>
+      <div className={cn("container", styles.container)}>
         <div className={styles.foot}>
           <div className={styles.copyright}>
             Copyright © 2021 LACChain. All rights reserved
           </div>
           <div className={styles.note}>
-            We use cookies for better service.
-            {' '}
-            <a href="/#">Accept</a>
+            We use cookies for better service. <a href="/#">Accept</a>
           </div>
         </div>
       </div>
